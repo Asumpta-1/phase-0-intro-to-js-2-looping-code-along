c472787114b61036693cc3dce30e46e45b073f46
asumpta <sumgathoni@gmail.com>
2023-01-06 09:41:08 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+    function writeCards(nameOfFriends,event="birthday") {
+    let messages = []
+    for(let i = 0; i<nameOfFriends.length;i++) {
+     messages.push(`Thank you, ${nameOfFriends[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages 
+    }
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday") 
+
+function countDown(value) {
+while(value>=0) {
+    console.log(value)
+    value=value-1
+}
+}
